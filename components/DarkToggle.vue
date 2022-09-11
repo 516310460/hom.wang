68e6037fe0387b559285be0934d02892fb8697d0
HomWang <ajake123123@gmail.com>
2022-09-11 15:21:02 +0800

    Optimizing Accessibility

--- components/DarkToggle.vue
+++ components/DarkToggle.vue
@@ -7,7 +7,7 @@ function toggleDark() {
 </script>
   
 <template>
-  <button dark="text-white" light="text-black" hover="opacity-100" class="text-fs24 opacity-50 transition delay-50 !outline-none" @click="toggleDark">
+  <button aria-label="DarkToggle" dark="text-white" light="text-black" hover="opacity-100" class="text-fs24 opacity-50 transition delay-50 !outline-none" @click="toggleDark">
     <div class="dark:i-carbon-moon i-carbon-sun text-fs24" />
   </button>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
       </NuxtLink>
       <div class="flex items-center space-x-4">
         <NuxtLink dark="text-white" light="text-black" hover="opacity-100" class="opacity-50 transition delay-50 flex items-center space-x-1"
+          :aria-label="$t('header.在线聊天')"
           to="https://socket.hom.wang">
           <i i-carbon-chat></i>
           <span>{{ $t('header.在线聊天') }}</span>  
@@ -17,7 +18,7 @@
           <NuxtLink :to="switchLocalePath(locale.code) || ''">{{ locale.name }}</NuxtLink>
         </span>
         <DarkToggle />
-        <a dark="text-white" light="text-black" hover="opacity-100" class="text-fs24 opacity-50 transition delay-50" i-carbon-logo-github href="https://github.com/516310460/hom.wang" target="_blank" />
+        <a dark="text-white" light="text-black" hover="opacity-100" class="text-fs24 opacity-50 transition delay-50"  i-carbon-logo-github :aria-label="$t('header.阅读王弘更多的信息')" to="https://github.com/516310460/hom.wang" target="_blank" />
       </div>
     </w-header>
     <div class="max-w-1000px mx-auto px-6 py-10 space-y-6">
@@ -27,7 +28,7 @@
       </p>
       <p dark="text-[#ccc]" light="text-black opacity-50" class="text-fs16 leading-6">{{ $t('home.介绍') }}</p>
       <div class="text-fs16">
-        <span dark="text-[#ccc]" light="text-black opacity-50">{{ $t('home.创建了~') }}</span><NuxtLink dark="text-white" light="text-black" hover="text-primary-500 border-primary-500" class="border-b transition delay-50" to="https://github.com/516310460/NuxtUI">nuxt-ui</NuxtLink>, <NuxtLink dark="text-white" light="text-black" hover="text-primary-500 border-primary-500" class="border-b transition delay-50" to="https://github.com/516310460/pinia-nuxt-persist">pinia-nuxt-persist</NuxtLink><span dark="text-[#ccc]" light="text-black opacity-50">{{ $t('home.~等') }}</span>
+        <span dark="text-[#ccc]" light="text-black opacity-50">{{ $t('home.创建了~') }}</span><NuxtLink dark="text-white" light="text-black" hover="text-primary-500 border-primary-500" class="border-b transition delay-50" :aria-label="$t('header.阅读王弘更多的信息')" to="https://github.com/516310460/NuxtUI">nuxt-ui</NuxtLink>, <NuxtLink dark="text-white" light="text-black" hover="text-primary-500 border-primary-500" class="border-b transition delay-50" :aria-label="$t('header.阅读王弘更多的信息')" to="https://github.com/516310460/pinia-nuxt-persist">pinia-nuxt-persist</NuxtLink><span dark="text-[#ccc]" light="text-black opacity-50">{{ $t('home.~等') }}</span>
       </div>
       <p dark="text-[#ccc]" light="text-black opacity-50" class="text-fs16 leading-6">{{ $t('home.热情所在') }}</p>
       <p dark="text-[#ccc]" light="text-black opacity-50" class="text-fs16 leading-6">{{ $t('home.兴趣爱好') }}</p>
