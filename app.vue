1a5de85276177034e3e5aec2b1692a7db22d7364
HomWang <ajake123123@gmail.com>
2022-09-11 14:38:37 +0800

    Add sw client

--- app.vue
+++ app.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
+const { t } = useI18n()
 useHead({
-  title: 'Hom Wang',
+  title: t('layouts.title'),
   // link: [
   //   {
   //     rel: 'icon', type: 'image/png', href: '/nuxt.png',
   //   },
   // ],
 })
+if (process.client) {
+  window.addEventListener('load', () => {
+    if (!('serviceWorker' in navigator)) {
+      throw new Error('serviceWorker is not supported in current browser!')
+    }
+    navigator.serviceWorker.register('/sw.js')
+  })
+}
 </script>
   
 <template>
